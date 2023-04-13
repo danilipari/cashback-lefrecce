3ba6328ac16ea485eadada08d471bf41d26fb57d
Dani Lipari <lipari.assistenza@gmail.com>
2023-04-13 04:15:25 +0200

    feat: :art: Upgrade structure
    Change type:module for use import ES6 + add env

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import http from 'node:http';
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const app = express();
+const server = http.createServer(app);
+
+const middleware = [
+  cors(),
+  helmet(),
+  express.urlencoded({ extended: true }),
+  express.json(),
+  morgan('combined')
+];
+
+app.use(middleware);
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello, cashback-lefrecce mode:${process.env.NODE_ENV} coming soon!\n`);
+});
+
+const hostname = process.env.HOST || 'www.cashback-lefrecce.it';
+const port = process.env.PORT || 3000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
