06e602efc754b71535e934d1dbc72801aae24821
Dani Lipari <lipari.assistenza@gmail.com>
2023-04-16 04:26:25 +0200

    feat: :sparkles: utils + dynamic env + dynamic html replacement + contentSecurityPolicy & more ...

--- index.mjs
+++ index.mjs
@@ -4,21 +4,63 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import Utils from './utils/utils.mjs';
+const utils = new Utils();
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const dynamicEnvs = {
+  YEAR: new Date().getFullYear(),
+};
+
+utils.patch_env(process.env, dynamicEnvs) && dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app = express();
 const server = http.createServer(app);
 
 const middleware = [
   cors(),
-  helmet(),
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+      "script-src": ["'self'", "https: data:"],
+    },
+  }),
   express.urlencoded({ extended: true }),
   express.json(),
-  morgan('combined')
+  morgan('combined'),
 ];
 
 app.use(middleware);
+
+app.get('/images/:file', (req, res) => {
+  utils.parseImage(process.env, `${req.params.file}`, (error, data) => {
+    if (error) {
+      res.status(500).send(error.message.split(', open \'./static/')[0]);
+      return;
+    }
+
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get('/landing', (req, res) => {
+  fs.readFile(`${process.env.HTML_DIR}coming_soon.html`, (error, data) => {
+    if (error) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('File non trovato');
+      return res.end();
+    }
+
+    data = utils.htmlReplaceEnv(process.env, data);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+});
 
 app.get('/', (req, res) => {
   res.statusCode = 200;
---
+++ utils/utils.mjs
@@ -0,0 +1,47 @@
+import fs from 'fs';
+
+class Utils {
+
+  patch_env = (env, newEnvVars) => {
+    let envFile = fs.readFileSync(`.env.${env.NODE_ENV}`, 'utf8');
+    const envVars = {};
+    envFile.split('\n').forEach(line => {
+      const [key, value] = line.split('=');
+      envVars[key] = value;
+    });
+    Object.assign(envVars, newEnvVars);
+    let envString = '';
+    for (const key in envVars) {
+      if (envVars[key]) {
+        envString += `${key}=${envVars[key]}\n`;
+      }
+    }
+    fs.writeFileSync(`.env.${env.NODE_ENV}`, envString);
+
+    return true;
+  }
+
+  parseImage = (env, filename, callback) => {
+    fs.readFile(`${env.INT_IMG_DIR}${filename}`, (error, data) => {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+    });
+  };
+
+  htmlReplaceEnv = (env, data) => {
+    let convertedData = data.toString();
+    const ENVS = Object.keys(env);
+
+    for (let key of ENVS) {
+      const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
+      convertedData = convertedData.replace(regex, process.env[key]);
+    };
+    return Buffer.from(convertedData, 'utf-8');
+  }
+
+}
+
+export default Utils;
