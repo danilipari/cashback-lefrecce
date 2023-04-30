877a4fce3b7067124d5d00ab5f110bf3ff24b5f5
Dani Lipari <lipari.assistenza@gmail.com>
2023-05-01 00:58:40 +0200

    add redis complete

--- index.mjs
+++ index.mjs
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import rateLimit from 'express-rate-limit';
+import { createClient } from 'redis';
 
 import Utils from './utils/utils.mjs';
 const utils = new Utils();
@@ -46,6 +47,21 @@ if (process.env.NODE_ENV === 'production') {
 app.disable('x-powered-by');
 app.use(middleware);
 
+const redisMiddleware = (req, res, next) => {
+  const redisClient = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT
+    }
+  });
+  redisClient.on('error', err => console.log('Redis Server Error', err));
+  redisClient.connect();
+  // console.log("redisMiddleware -->", redisClient);
+  req.redis$ = redisClient;
+  next();
+}
+
 const server = http.createServer(app);
 
 app.get('/images/:file', (req, res) => {
@@ -61,8 +77,40 @@ app.get('/images/:file', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
-  fs.readFile(`${process.env.HTML_DIR}coming_soon.html`, (error, data) => {
+app.get('/redis', redisMiddleware, async (req, res) => {
+  const redisClient = req.redis$;
+  try {
+    const result = await redisClient.set('test', 'scritto');
+    console.log(result); // 'OK'
+
+    const value = await redisClient.get('test');
+    console.log(value); // 'scritto'
+  } catch (err) {
+    console.error(err);
+  } finally {
+    redisClient.disconnect(); // disconnette il client Redis
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Test Redis!`);
+});
+
+
+app.get('/', redisMiddleware, async (req, res) => {
+  const redis$ = await req.redis$;
+  const cacheData = await redis$.get(`${process.env.HTML_DIR}coming_soon.html`);
+
+  if (cacheData) {
+    console.log("--------------> dentro alla cache redis");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(cacheData);
+    await redis$.disconnect();
+    return res.end();
+  }
+
+  console.log("--------------> fuori alla cache redis");
+  fs.readFile(`${process.env.HTML_DIR}coming_soon.html`, async (error, data) => {
     if (error) {
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.write('File non trovato');
@@ -70,6 +118,8 @@ app.get('/', (req, res) => {
     }
 
     data = utils.htmlReplaceEnv(process.env, data);
+    await redis$.set(`${process.env.HTML_DIR}coming_soon.html`, data);
+    await redis$.disconnect();
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
