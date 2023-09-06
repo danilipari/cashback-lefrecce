208d5bf959561219d0c1c43317c3786624a368b8
Dani Lipari <lipari.assistenza@gmail.com>
2023-09-06 02:46:26 +0200

    refactor: add more conf pm2 ecosystem.config + clean package.json all app

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,10 +4,30 @@ module.exports = {
       name: 'main-app',
       script: './index.mjs',
       node_args: '--experimental-modules',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
     {
       name: 'nest-app',
       script: './bot/dist/main.js',
-    },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    }
   ],
 };
