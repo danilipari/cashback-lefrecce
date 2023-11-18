e8c42a982f87849367a9b5823ddb192b37f6a0d8
Dani Lipari <dani.lipari@dontouch.ch>
2023-11-18 17:34:16 +0100

    chore(configs): update config for deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,19 +15,19 @@ module.exports = {
         NODE_ENV: 'development',
       },
     },
-    {
-      name: 'nest-app',
-      script: './bot/dist/main.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-      },
-      env_development: {
-        NODE_ENV: 'development',
-      },
-    }
+    // {
+    //   name: 'nest-app',
+    //   script: './bot/dist/main.js',
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '1G',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //   },
+    //   env_development: {
+    //     NODE_ENV: 'development',
+    //   },
+    // }
   ],
 };
